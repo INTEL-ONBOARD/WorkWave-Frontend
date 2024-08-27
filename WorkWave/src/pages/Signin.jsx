3f507f61c2b32e9ef633ce2e7477199e32762427
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-08-27 09:01:18 +0530

    fixes

--- WorkWave/src/pages/Signin.jsx
+++ WorkWave/src/pages/Signin.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './SignIn.css'; 
 
 const SignIn = () => {
     return (
-        <div>
+        <div className="signin-container">
             <h1>Sign In</h1>
-            <p>This is the Sign In page.</p>
+            <form className="signin-form">
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit" className="signin-button">Sign In</button>
+            </form>
         </div>
     );
 }
